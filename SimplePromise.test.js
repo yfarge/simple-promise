f673d90bec9202e597cc846d66569245c5debbeb
Yousef Farge <yousef.farge@gmail.com>
2024-10-23 22:08:20 -0500

    test(SimplePromise): added unit tests for static methods: resolve, reject, all, allSettled, race, any

--- SimplePromise.test.js
+++ SimplePromise.test.js
@@ -60,6 +60,12 @@ describe('catch', () => {
             })
             .catch((reason) => expect(reason).toBe(4))
     })
+
+    it('should throw an `UncaughtPromiseError` if a promise is rejected and not caught', async () => {
+        await expect(
+            new SimplePromise((_, reject) => reject(ERROR_MESSAGE))
+        ).rejects.toThrowError()
+    })
 })
 
 describe('finally', () => {
@@ -90,5 +96,96 @@ describe('finally', () => {
             .then((value) => value)
             .catch((reason) => reason)
             .finally((value) => expect(value).toBeUndefined())
+    })
+})
+
+describe('static methods', () => {
+    describe('resolve', () => {
+        it('should resolve with the correct value', async () => {
+            await SimplePromise.resolve(DEFAULT_VALUE).then((value) =>
+                expect(value).toBe(DEFAULT_VALUE)
+            )
+        })
+    })
+
+    describe('reject', () => {
+        it('should reject with the correct value', async () => {
+            await SimplePromise.reject(ERROR_MESSAGE).catch((reason) =>
+                expect(reason).toBe(ERROR_MESSAGE)
+            )
+        })
+    })
+
+    describe('all', () => {
+        it('should return a promise that resolves to array of values if all promises are successful', async () => {
+            await SimplePromise.all([
+                new SimplePromise((resolve) => resolve(0)),
+                new SimplePromise((resolve) => resolve(1)),
+                new SimplePromise((resolve) => resolve(2)),
+            ]).then((values) => expect(values).toEqual([0, 1, 2]))
+        })
+
+        it('should return a promise that rejects to the first reject reason if any of the promises reject', async () => {
+            await SimplePromise.all([
+                new SimplePromise((resolve) => resolve(0)),
+                new SimplePromise((resolve) => resolve(1)),
+                new SimplePromise((reject) => reject(ERROR_MESSAGE)),
+            ]).catch((value) => expect(value).toBe(ERROR_MESSAGE))
+        })
+    })
+
+    describe('allSettled', () => {
+        it('should return a promise that resolves to an array of objects', async () => {
+            await SimplePromise.allSettled([
+                new SimplePromise((resolve) => resolve(0)),
+                new SimplePromise((_, reject) => reject(1)),
+                new SimplePromise((resolve) => resolve(2)),
+                new SimplePromise((_, reject) => reject(3)),
+                new SimplePromise((resolve) => resolve(4)),
+            ]).then((values) =>
+                expect(values).toEqual([
+                    { status: 'fulfilled', value: 0 },
+                    { status: 'rejected', reason: 1 },
+                    { status: 'fulfilled', value: 2 },
+                    { status: 'rejected', reason: 3 },
+                    { status: 'fulfilled', value: 4 },
+                ])
+            )
+        })
+    })
+
+    describe('race', () => {
+        it('should return a promise that settles with the eventual state of the first promise that settles', async () => {
+            await SimplePromise.race([
+                new SimplePromise((resolve) => resolve(0)),
+                new SimplePromise((resolve) => resolve(1)),
+            ]).then((value) => expect(value).toBe(0))
+
+            await SimplePromise.race([
+                new SimplePromise((reject) => reject(0)),
+                new SimplePromise((resolve) => resolve(1)),
+            ]).then((value) => expect(value).toBe(0))
+        })
+    })
+
+    describe('any', () => {
+        it('should return a promise that resolves to the first fulfilled promise value', async () => {
+            await SimplePromise.any([
+                new SimplePromise((resolve) => resolve(0)),
+                new SimplePromise((resolve) => resolve(1)),
+                new SimplePromise((resolve) => resolve(2)),
+            ]).then((value) => expect(value).toBe(0))
+        })
+
+        it('should reject to an `AggregateError` if all of the promises are rejected', async () => {
+            await SimplePromise.any([
+                new SimplePromise((_, reject) => reject(0)),
+                new SimplePromise((_, reject) => reject(1)),
+                new SimplePromise((_, reject) => reject(2)),
+            ]).catch((e) => {
+                expect(e).toBeInstanceOf(AggregateError)
+                expect(e.errors).toEqual([0, 1, 2])
+            })
+        })
     })
 })
